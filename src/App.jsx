69243bfb92fc749e086f114e4e0b2bee43fb258c
Gokul Menon <gokul.menon@free-now.com>
2023-12-10 21:56:32 +0100

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-
 import React from 'react';
 import CardContainer from './Cards/card';
 import Search from './search/Search';
  
 function App() {
-  return (
-  
-<>
-<div className='search'>
-  <Search />
-
-</div>
-
-    <CardContainer />
-</>
-
-
-   
-  );
- }
+ return (
+   <>
+     <div className='search'>
+       <Search />
+     </div>
+     <CardContainer />
+     <div className='footer'>
+       Made in Berlin, with ❤️
+     </div>
+   </>
+ );
+}
  
- 
- export default App;
- 
+export default App;
