de2ab8776ab51fcbae69c87578ad0ce4998fb9f0
Gokul Menon <gokul.menon@free-now.com>
2023-12-11 01:17:20 +0100

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@
 import React from 'react';
 import CardContainer from './Cards/card';
 import Search from './search/Search';
+
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="header">Find your optimal workplace in the city's cafes ☕</h1>
+      <p className="header-para">I'll be working from:</p>
+    </div>
+  );
+ };
+ 
  
 function App() {
   return (
     <>
+    <Header />
       <div className='search'>
         <Search />
       </div>
